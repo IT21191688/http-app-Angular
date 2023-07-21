19fec9b7a11bc9067761f4c950168f42900e5b1e
IT21191688 <IT21191688@my.sliit.lk>
2023-07-22 01:31:58 +0530

    delete success

--- src/app/components/delete/delete.component.ts
+++ src/app/components/delete/delete.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-delete',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./delete.component.scss']
 })
 export class DeleteComponent {
+
+  constructor(private httpModule : HttpClient, private _snackBar: MatSnackBar) {
+  }
+  list:Array<any> = []
+
+  deleteData(id: any){
+    if(confirm('are you sure ' + id)){
+      this.httpModule.delete<any>('https://jsonplaceholder.typicode.com/posts/'+id)
+        .subscribe(response => {
+          if(response){
+            this._snackBar.open('Deleted','close',{
+              horizontalPosition: "end",
+              verticalPosition: 'bottom',
+              duration: 5000,
+              direction: 'ltr'
+            })
+          }
+        });
+    }
+  }
+  ngOnInit(): void {
+    this.httpModule.get<any>('https://jsonplaceholder.typicode.com/posts')
+      .subscribe(response => {
+        this.list = response
+      });
+  }
 
 }
