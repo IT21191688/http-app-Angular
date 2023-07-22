e8919b04f1f89dbaa61a46c005ce9b25fed8efcf
IT21191688 <IT21191688@my.sliit.lk>
2023-07-22 21:44:10 +0530

    firebase config

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA19zG6msEyJzZXd97Hh3JsXaG1webnaiw",
+  authDomain: "http-app-84e31.firebaseapp.com",
+  projectId: "http-app-84e31",
+  storageBucket: "http-app-84e31.appspot.com",
+  messagingSenderId: "341106126692",
+  appId: "1:341106126692:web:aea31f5889ff71e97f5471",
+  measurementId: "G-ZBXN6CWVBQ"
+  }
+}
